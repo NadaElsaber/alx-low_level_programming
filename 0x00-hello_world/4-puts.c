c4438c4652c6c023570e92a02eccdfa9eafa49b2
NadaElsaber <nadamelsaber@gmail.com>
2022-12-11 05:47:27 -0800

    4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *Description: puts "Programming is like building a multilingual puzzle
+ *Return: Always 0 (Success)
+ */
+
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+
+	puts("\n");
+
+	return (0);
+}
