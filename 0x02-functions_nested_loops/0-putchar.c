b03fe39a5c1546255fadae3af85f6417ba639197
NadaElsaber <nadamelsaber@gmail.com>
2022-12-13 12:55:34 -0800

    0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,14 +8,14 @@
 
 int main(void)
 {
-	char myvar[] = "_putchar";
-	int i = 0;
-
-	while (myvar[i] != '\0')
-	{
-		_putchar(myvar[i]);
-		i++;
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 	return (0);
 }
